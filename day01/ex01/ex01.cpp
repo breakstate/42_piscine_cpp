a9b83c3912e2a3ae431df549c4c6b61006d8f602
Brenton MOODLEY <bmoodley@e4r16p2.wethinkcode.co.za>
2018-06-06 08:30:51 +0200

    ex01

---
+++ day01/ex01/ex01.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+void memoryLeak()
+{
+    std::string* panthere = new std::string("String panthere");
+    std::cout << *panthere << std::endl;
+    delete panthere;
+    return;
+}
